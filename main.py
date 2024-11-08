861772e0e7ce7aa4f1d6423f03f5e3045afabacc
Jean Tessier <jean@jeantessier.com>
2024-11-08 11:57:37 -0500

    Add Google Gemini
    Based on https://pypi.org/project/langchain-google-genai/

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
-model = ChatOpenAI(model="gpt-4o-mini")
+# model = ChatOpenAI(model="gpt-4o-mini")
+model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
 
 system_template = "Translate the following into {language}:"
 prompt_template = ChatPromptTemplate.from_messages(
@@ -16,4 +18,6 @@ parser = StrOutputParser()
 
 chain = prompt_template | model | parser
 
-chain.invoke({"language": "italian", "text": "hi"})
+result = chain.invoke({"language": "italian", "text": "hi"})
+
+print(result)

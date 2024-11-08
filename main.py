9652def32b5aa78ffaac000c9c30a8c11b0b5519
Jean Tessier <jean@jeantessier.com>
2024-11-08 12:11:42 -0500

    Parse command line args to select model

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
@@ -7,9 +8,18 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
-# model = ChatOpenAI(model="gpt-4o-mini")
-model = ChatAnthropic(model="claude-3-5-sonnet-20240620")
-# model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+argument_parser = ArgumentParser()
+argument_parser.add_argument("-m", "--model", help="Which model to connect to", default="openai")
+args = argument_parser.parse_args()
+
+if args.model == "openai":
+    model = ChatOpenAI(model="gpt-4o-mini")
+elif args.model == "anthropic":
+    model = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+elif args.model == "google":
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+else:
+    raise ValueError(f"Unknown model {args.model}")
 
 system_template = "Translate the following into {language}:"
 prompt_template = ChatPromptTemplate.from_messages(

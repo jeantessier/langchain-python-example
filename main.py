67e2013544620619608eb79d653358c6c3d89b67
Jean Tessier <jean@jeantessier.com>
2024-11-08 11:44:43 -0500

    Use chain with prompt template

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from dotenv import load_dotenv
-from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
 model = ChatOpenAI(model="gpt-4o-mini")
 
-messages = [
-    SystemMessage(content="Translate the following from English into Italian"),
-    HumanMessage(content="hi!"),
-]
-
-result = model.invoke(messages)
+system_template = "Translate the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
+)
 
 parser = StrOutputParser()
-parser.invoke(result)
+
+chain = prompt_template | model | parser
+
+chain.invoke({"language": "italian", "text": "hi"})

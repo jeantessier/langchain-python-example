18403f5db63c31e41b3a27abbb57d21b85eb6ee2
Jean Tessier <jean@jeantessier.com>
2025-12-27 18:47:02 -0800

    Replace if-cascade with structural pattern matching

--- main.py
+++ main.py
@@ -8,27 +8,31 @@ from langchain_anthropic import ChatAnthropic
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_deepseek import ChatDeepSeek
 
+def create_chat_model(model):
+    match model:
+        case "openai" | "":
+            return ChatOpenAI(model=os.environ["OPENAI_MODEL"], api_key=os.environ["OPENAI_API_KEY"])
+        case "anthropic":
+            return ChatAnthropic(model=os.environ["ANTHROPIC_MODEL"], api_key=os.environ["ANTHROPIC_API_KEY"])
+        case "google":
+            return ChatGoogleGenerativeAI(model=os.environ["GOOGLE_MODEL"], api_key=os.environ["GOOGLE_API_KEY"])
+        case "deepseek":
+            return ChatDeepSeek(model=os.environ["DEEPSEEK_MODEL"], api_key=os.environ["DEEPSEEK_API_KEY"])
+        case _:
+            raise ValueError(f"Unknown model {args.model}")
+
 load_dotenv()
 
 argument_parser = ArgumentParser()
 argument_parser.add_argument("-m", "--model", help="Which model to connect to", default="openai")
 args = argument_parser.parse_args()
 
-if args.model == "openai":
-    model = ChatOpenAI(model=os.environ["OPENAI_MODEL"], api_key=os.environ["OPENAI_API_KEY"])
-elif args.model == "anthropic":
-    model = ChatAnthropic(model=os.environ["ANTHROPIC_MODEL"], api_key=os.environ["ANTHROPIC_API_KEY"])
-elif args.model == "google":
-    model = ChatGoogleGenerativeAI(model=os.environ["GOOGLE_MODEL"], api_key=os.environ["GOOGLE_API_KEY"])
-elif args.model == "deepseek":
-    model = ChatDeepSeek(model=os.environ["DEEPSEEK_MODEL"], api_key=os.environ["DEEPSEEK_API_KEY"])
-else:
-    raise ValueError(f"Unknown model {args.model}")
-
 system_template = "Translate the following into {language}:"
 prompt_template = ChatPromptTemplate.from_messages(
     [("system", system_template), ("user", "{text}")]
 )
+
+model = create_chat_model(args.model)
 
 parser = StrOutputParser()
 
